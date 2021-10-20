44f4edc0a06e4182beada1a761ccc4b5674922cc
amadyanissa <amalya.adyanissa@gmail.com>
2021-10-20 11:42:15 +0700

    add metadata

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,8 @@ function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
       <Head>
+        <title>Betflix</title>
+        <link href="/betflix-icon.ico" rel="shortcut icon" type="image/x-icon" />
         <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
       </Head>
       <Component {...pageProps} />
