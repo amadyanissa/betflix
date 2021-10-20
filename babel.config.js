b73ac092d394102c6e23d164c615d7e7d6d6b9fe
amadyanissa <amalya.adyanissa@gmail.com>
2021-10-20 10:19:34 +0700

    fix configuration to deploy

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     presets:["@babel/preset-env", "@babel/preset-react", 'next/babel'],
-    plugins: ["@babel/plugin-proposal-class-properties", "@babel/plugin-syntax-dynamic-import"],
+    plugins: ["@babel/plugin-proposal-class-properties", "@babel/plugin-syntax-dynamic-import", ],
     
 }
